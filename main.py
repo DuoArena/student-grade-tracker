15cd17357866363fc594e51b5f79cd338108b827
Joaquin <joaquincrist.m_cis@gemsed.com>
2026-07-03 18:17:52 +0400

    Clean up grade tracker and improve output formatting, and finalized the code

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ numstudents= int(input("How many students do you have?: "))
 numsubjects =int(input("How many subjects do they study?"))
 
 names = ["" for _ in range(numstudents)]
-grades = [[0 for _ in range(numsubjects + 1)]
+grades = [[0 for _ in range(numsubjects)]
           for _ in range(numstudents)]
-# initializing names list and grades 2d arrays AND made room for the average grades
+# initializing names list and grades 2d arrays
 finalgrade = ["" for _ in range(numstudents)]
 #initializing separate array for final grade
+
+
 for studentcount in range(numstudents):
     total= 0
     name = str(input("Enter Student name:"))
@@ -22,7 +24,7 @@ for studentcount in range(numstudents):
     averagegrade = int(round((total / numsubjects),0))
     #made average grade calculator 
     names[studentcount] = name
-    grades[studentcount][numsubjects + 1] = averagegrade
+    grades[studentcount].append(averagegrade)
     if averagegrade >= 80:
         print("This student has then EXCELLENT")
         finalgrade[studentcount] = str("EXCELLENT")
@@ -33,6 +35,24 @@ for studentcount in range(numstudents):
         print("This student has underperformed, requires more help")
         finalgrade[studentcount] =  str("UNDERPERFORMING")
     #Implement grade assignment based on average
+
+#Start displaying their grades and Such
+
+print("=====================================")
+print("Student Report")
+print("=====================================")
+
+for i in range(numstudents):
+    marks = grades[i][:-1]
+    avg = grades[i][-1]
+    #Stores all the values in the grade array except for the last and avg stores only the last thing in the array(aka average)
+
+    print(names[i])
+    print("Marks:", marks)
+    print("Average:", avg)
+    print("Result:", finalgrade[i])
+    print("----------------------------------")
+
 
 
 
